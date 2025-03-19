e3905d719dfb9096d656d6e2d343b62667ea2b25
AIProGuru <ai.pro961212@gmail.com>
2025-03-19 09:47:33 -0700

    deleted some resources for test

--- src/components/private-route/PrivateRoute.comp.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-export const PrivateRoute = ({user, children}) => {
-    const token = localStorage.getItem('access-token')
-    if(!token)
-        return <Navigate to="/" />
-    return children;
-}
